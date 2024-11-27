b97844f8ba25ea1f1d4088172a380219dbda6c77
pikac <643645927@qq.com>
2024-11-27 17:57:41 +0800

    数据绑定 显示不同项目模板

--- PKEngine/PKEngineEditor/GameProject/NewProjectViewModel.cs
+++ PKEngine/PKEngineEditor/GameProject/NewProjectViewModel.cs
@@ -1,39 +1,95 @@
 ﻿using PKEngineEditor.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using PKEngineEditor.Utilities;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.Serialization;
 
 namespace PKEngineEditor.GameProject
 {
+    [DataContract]
+    public class ProjectTemplate
+    {
+        [DataMember]
+        public string? ProjectType { get; set; }
+        [DataMember]
+        public string? ProjectFile { get; set; }
+        [DataMember]
+        public List<string>? Folders { get; set; }
+
+        public byte[]? Icon { get; set; }
+        public byte[]? ScreenShot { get; set; }
+        public string? IconFilePath { get; set; }
+        public string? ScreenShotFilePath { get; set;}
+        public string? ProjectPath { get; set; }
+    }
+
     class NewProjectViewModel : ViewModelBase
     {
-        private string _name = "NewProject";
-        public string Name
+        private readonly string _templatePath = @"..\..\PKEngineEditor\ProjectTemplates";
+
+        private string _projectName = "NewProject";
+        public string ProjectName
         {
-            get => _name;
+            get => _projectName;
             set
             {
-                if (value != _name)
+                if (value != _projectName)
                 {
-                    _name = value;
-                    OnPropertyChanged(nameof(Name));
+                    _projectName = value;
+                    OnPropertyChanged(nameof(ProjectName));
                 }
             }
         }
 
-        private string _path = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\PKProject\";
-        public string Path
+        private string _projectPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\PKProject\";
+        public string ProjectPath
         {
-            get => _path;
+            get => _projectPath;
             set
             {
-                if (value != _path)
+                if (value != _projectPath)
                 {
-                    _path = value;
-                    OnPropertyChanged(nameof(Path));
+                    _projectPath = value;
+                    OnPropertyChanged(nameof(ProjectPath));
                 }
+            }
+        }
+
+        private ObservableCollection<ProjectTemplate> _projectTemplates = new ObservableCollection<ProjectTemplate>();
+
+        private ReadOnlyObservableCollection<ProjectTemplate> _readOnlyProjectTemplates;
+        public ReadOnlyObservableCollection<ProjectTemplate> ReadOnlyProjectTemplates
+        {
+            get
+            {
+                if (_readOnlyProjectTemplates == null)
+                {
+                    _readOnlyProjectTemplates = new ReadOnlyObservableCollection<ProjectTemplate>(_projectTemplates);
+                }
+                return _readOnlyProjectTemplates;
+            }
+        }
+
+        public NewProjectViewModel()
+        {
+            try
+            {
+                var templateFiles = Directory.GetFiles(_templatePath, "template.xml", SearchOption.AllDirectories);
+                foreach (var templateFile in templateFiles)
+                {
+                    var temp = Serializer.FromFile<ProjectTemplate>(templateFile);
+                    temp.IconFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(templateFile), "Icon.png"));
+                    temp.Icon = File.ReadAllBytes(temp.IconFilePath);
+                    temp.ScreenShotFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(templateFile), "ScreenShot.png"));
+                    temp.ScreenShot = File.ReadAllBytes(temp.ScreenShotFilePath);
+                    temp.ProjectPath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(templateFile), temp.ProjectFile));
+                    _projectTemplates.Add(temp);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
             }
         }
     }
