053481f9807df60b23e317df696d673eccda10a5
pikac <643645927@qq.com>
2024-11-27 11:52:42 +0800

    使用MVVM实现对界面界面元素的响应式控制

---
+++ PKEngine/PKEngineEditor/GameProject/NewProjectViewModel.cs
@@ -0,0 +1,40 @@
+﻿using PKEngineEditor.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PKEngineEditor.GameProject
+{
+    class NewProjectViewModel : ViewModelBase
+    {
+        private string _name = "NewProject";
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value != _name)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        private string _path = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\PKProject\";
+        public string Path
+        {
+            get => _path;
+            set
+            {
+                if (value != _path)
+                {
+                    _path = value;
+                    OnPropertyChanged(nameof(Path));
+                }
+            }
+        }
+    }
+}
