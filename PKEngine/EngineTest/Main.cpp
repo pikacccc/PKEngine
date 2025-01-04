2e1252b30cbddda6123d5fdbc3a43c99c0e0841f
pikac <643645927@qq.com>
2025-01-04 17:21:08 +0800

    Implement Engine Test

--- PKEngine/EngineTest/Main.cpp
+++ PKEngine/EngineTest/Main.cpp
@@ -1,20 +1,17 @@
-#include "Test.h"
+#pragma comment(lib,"engine.lib")
 
 #define TEST_ENTITY_COMPONENTS 1
 
 #if TEST_ENTITY_COMPONENTS
-class engine_test : public test
-{
-public:
-	bool initialize() override { return true; }
-	void run() override {}
-	void shutdown() override {}
-};
+#include "TestEntityComponents.h"
 #else
 #error One of the tests need to be enabled
 #endif
 
 int main() {
+#if _DEBUG
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
 	engine_test test{};
 
 	if (test.initialize()) {
