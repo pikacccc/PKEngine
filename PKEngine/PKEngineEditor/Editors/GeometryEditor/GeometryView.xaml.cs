dffe0b51f4530a513ba6ccbebde4ceb7d930585a
pikacccc <643645927@qq.com>
2026-08-06 18:43:59 +0800

    PKEngine/PKEngineEditor/Editors/GeometryEditor/GeometryView.xaml PKEngine/PKEngineEditor/Editors/GeometryEditor/GeometryView.xaml.cs

--- PKEngine/PKEngineEditor/Editors/GeometryEditor/GeometryView.xaml.cs
+++ PKEngine/PKEngineEditor/Editors/GeometryEditor/GeometryView.xaml.cs
@@ -1,11 +1,17 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Input;
 using System.Windows.Media.Media3D;
 
 namespace PKEngineEditor.Editors
 {
     public partial class GeometryView : UserControl
     {
+        private Point _clickPosition;
+        private bool  _capturedLeft;
+        private bool  _capturedRight;
+
         public GeometryView()
         {
             InitializeComponent();
@@ -56,6 +62,59 @@ namespace PKEngineEditor.Editors
 
             var visual = new ModelVisual3D { Content = modelGroup };
             viewport.Children.Add(visual);
+        }
+
+        private void OnGrid_Mouse_LBD(object sender, MouseButtonEventArgs e)
+        {
+            _clickPosition = e.GetPosition(this);
+            _capturedLeft  = true;
+            Mouse.Capture(sender as UIElement);
+        }
+
+        private void OnGrid_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!_capturedLeft && !_capturedRight) return;
+
+            var pos = e.GetPosition(this);
+            var d   = pos - _clickPosition;
+            if (_capturedLeft && !_capturedRight)
+            {
+                MoveCamera(d.X, d.Y, 0);
+            }
+            else if (!_capturedLeft && _capturedRight)
+            {
+                var vm      = DataContext as MeshRenderer;
+                var cp      = vm!.CameraPosition;
+                var yOffset = d.Y * 0.001 * Math.Sqrt(cp.X * cp.X + cp.Z * cp.Z);
+
+                vm.CameraTarget = new Point3D(vm.CameraTarget.X, vm.CameraTarget.Y + yOffset, vm.CameraTarget.Z);
+            }
+        }
+
+        private void MoveCamera(double dX, double dY, int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnGrid_Mouse_RBD(object sender, MouseButtonEventArgs e)
+        {
+            _capturedLeft = false;
+            if (!_capturedRight) Mouse.Capture(null);
+        }
+
+        private void OnGrid_Mouse_LBU(object sender, MouseButtonEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnGrid_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnGrid_Mouse_RBU(object sender, MouseButtonEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
